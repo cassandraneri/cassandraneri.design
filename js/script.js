11b08a348be5d4051a25dcdc63d6c4043efcadd7
cassandraneri <neric@hawaii.edu>
2023-04-27 14:35:07 -1000

    updated misc page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+// IMAGE SLIDER
+const navLeft = document.querySelector(".left");
+const navRight = document.querySelector(".right");
+
+const images = document.querySelector(".images");
+const colors = document.querySelector(".colored-backgrounds");
+
+let index = 0;
+
+function right() {
+     transform((index = index < 2 ? ++index : 0));
+}
+
+function left() {
+     transform((index = index > 0 ? --index : 3));
+}
+
+navLeft.addEventListener("click", left);
+navRight.addEventListener("click", right);
+
+function transform(index) {
+     images.style.transform = `translateX(-${index * 100}%)`;
+     colors.style.transform = `translateX(-${index * 100}%)`;
+}
+
 // OVERLAY NAV MENU SHOW HIDE
 const mymenubutton = document.querySelector('.menu-button');
 const mysitenav = document.querySelector('.site-header .site-nav');
